149fb7207dc931c9585f7d84b35d55eca7b558ac
shoravs <sharmasaurav142@gmail.com>
2022-10-04 08:04:29 +0530

    count Frequencies Of Char

---
+++ Strings/src/main/java/com/gfg/strings/FrequenciesOfChar.java
@@ -0,0 +1,18 @@
+package com.gfg.strings;
+
+public class FrequenciesOfChar {
+
+	public static void main(String[] args) {
+		String input = "rinki";
+		int[] count = new int[26];
+
+		for (int i = 0; i < input.length(); i++)
+			count[input.charAt(i) - 'a']++;
+
+		for (int i = 0; i < 26; i++)
+			if (count[i] > 0)
+				System.out.println((char) (i + 'a') + " " + count[i]);
+
+	}
+
+}
