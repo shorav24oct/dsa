f69e0a76b5eb7d19836912b38fe7f08964b25dca
shoravs <sharmasaurav142@gmail.com>
2022-10-05 12:13:25 +0530

    efficient approach for string palindrome

--- Strings/src/main/java/com/gfg/strings/PalindromeCheck.java
+++ Strings/src/main/java/com/gfg/strings/PalindromeCheck.java
@@ -3,11 +3,14 @@ package com.gfg.strings;
 public class PalindromeCheck {
 
 	public static void main(String[] args) {
-		String str = "ana";
+		String str = "a";
 
 		System.out.println("String reversed or not? : " + isPalindrome(str));
 	}
 
+	/*
+	 * naive solution
+	 */
 	static boolean isPalindrome(String str) {
 
 		StringBuilder stringBuilder = new StringBuilder(str);
@@ -15,6 +18,24 @@ public class PalindromeCheck {
 		stringBuilder.reverse();
 
 		return str.equals(stringBuilder.toString());
+	}
+
+	/*
+	 * efficient approach
+	 * time complexity Big-O(n)
+	 * space complexity Big-O(1)
+	 */
+	static boolean isPal(String str) {
+		int begin = 0;
+		int end = str.length() - 1;
+
+		while (begin < end) {
+			if (str.charAt(begin) != str.charAt(end))
+				return false;
+			begin++;
+			end--;
+		}
+		return true;
 	}
 
 }
