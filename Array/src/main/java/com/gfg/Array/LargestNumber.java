04ece925fab1483b97fdb15b16232423cf9db3a4
shoravs <sharmasaurav142@gmail.com>
2022-10-05 15:53:13 +0530

    naive and efficient sol for finding largest no from array

---
+++ Array/src/main/java/com/gfg/Array/LargestNumber.java
@@ -0,0 +1,43 @@
+package com.gfg.Array;
+
+public class LargestNumber {
+
+	/*
+	 * naive approach Time Complexity Big-O(n*n)
+	 */
+	public static int largestNoNaive(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			boolean flag = true;
+			for (int j = 0; j < arr.length; j++) {
+				if (arr[i] < arr[j]) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag == true)
+				return i;
+		}
+		return -1;
+	}
+
+	/*
+	 * efficient solution time complexity Big-O(n) 
+	 */
+	public static int largestNoEfficient(int[] arr) {
+		int res = 0;
+
+		for (int i = 1; i < arr.length; i++)
+			if (arr[i] > arr[res])
+				res = i;
+		
+		return res;
+
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 500, 8, 100, 70, 2000 };
+
+		System.out.println(largestNoEfficient(arr));
+	}
+
+}
