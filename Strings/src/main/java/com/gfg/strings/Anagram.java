8ad78295152527e5db1f4524f47243b9f5814fea
shoravs <sharmasaurav142@gmail.com>
2022-10-05 15:13:14 +0530

    efficient approach for checking string is anagram or not

--- Strings/src/main/java/com/gfg/strings/Anagram.java
+++ Strings/src/main/java/com/gfg/strings/Anagram.java
@@ -22,14 +22,27 @@ public class Anagram {
 	}
 
 	/*
-	 * 
+	 * efficient approach
 	 */
 	public static boolean isAna(String str1, String str2) {
+		if (str1.length() != str2.length())
+			return false;
 
+		int[] count = new int[256];
+
+		for (int i = 0; i < str1.length(); i++) {
+			count[str1.charAt(i)]++;
+			count[str2.charAt(i)]--;
+		}
+
+		for (int i = 0; i < 256; i++)
+			if (count[i] != 0)
+				return false;
+		return true;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(isAnagram("anu", "una"));
+		System.out.println(isAna("una", "ana"));
 	}
 
 }
--- Strings/src/main/java/com/gfg/strings/FrequenciesOfChar.java
+++ Strings/src/main/java/com/gfg/strings/FrequenciesOfChar.java
@@ -3,7 +3,9 @@ package com.gfg.strings;
 public class FrequenciesOfChar {
 
 	public static void main(String[] args) {
+		
 		String input = "geeksforgeeks";
+		
 		int[] count = new int[26];
 
 		for (int i = 0; i < input.length(); i++)
