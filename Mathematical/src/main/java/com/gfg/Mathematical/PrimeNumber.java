fef694af33046c2ee8e98ed2e2067bc5254d3231
shoravs <sharmasaurav142@gmail.com>
2024-01-23 07:27:21 +0530

    add efficient solution

--- Mathematical/src/main/java/com/gfg/Mathematical/PrimeNumber.java
+++ Mathematical/src/main/java/com/gfg/Mathematical/PrimeNumber.java
@@ -3,7 +3,8 @@ package com.gfg.Mathematical;
 public class PrimeNumber {
 
 	public static void main(String[] args) {
-		System.out.println(checkPrime(7));
+		//System.out.println(checkPrime(701));
+		System.out.println(checkPrimeEfficient(9));
 	}
 
 	// naive method
@@ -13,6 +14,19 @@ public class PrimeNumber {
 			return false;
 
 		for (int i = 2; i < no; i++)
+			if (no % i == 0)
+				return false;
+
+		return true;
+	}
+
+	// efficient solution
+
+	private static boolean checkPrimeEfficient(int no) {
+		if (no == 1)
+			return false;
+
+		for (int i = 2; i * i <= no; i++)
 			if (no % i == 0)
 				return false;
 
