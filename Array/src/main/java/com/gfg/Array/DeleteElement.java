9af16f6a1a57a70092fe5eebcfe5014f6a3dd210
shoravs <sharmasaurav142@gmail.com>
2024-01-27 22:56:17 +0530

    delete an element from an array

--- Array/src/main/java/com/gfg/Array/DeleteElement.java
+++ Array/src/main/java/com/gfg/Array/DeleteElement.java
@@ -7,20 +7,25 @@ public class DeleteElement {
 	public static void main(String[] args) {
 		int arr[] = { 5, 7, 3, 20, 1 };
 		System.out.println("Before Delete =>" + Arrays.toString(arr));
-		deleteAnElement(arr, arr.length, 3);
+		System.out.println(deleteAnElement(arr, arr.length, 3));
+		System.out.println("After Delete =>" + Arrays.toString(arr));
 	}
 
 	private static int deleteAnElement(int[] arr, int length, int noToBeDelete) {
-		
+
 		int i = 0;
-		
+
 		for (i = 0; i < length; i++)
 			if (arr[i] == noToBeDelete)
 				break;
-		
-		
 
-		return -1;
+		if (i == length)
+			return length;
+
+		for (int j = i; j < length - 1; j++)
+			arr[j] = arr[j + 1];
+
+		return (length - 1);
 	}
 
 }
