fea9c3b3e1e0d8463a0eedb29e43052754b743ff
shoravs <sharmasaurav142@gmail.com>
2024-04-23 07:36:17 +0530

    substring and palindrome check

--- Strings/src/main/java/com/gfg/strings/PalindromeCheck.java
+++ Strings/src/main/java/com/gfg/strings/PalindromeCheck.java
@@ -3,9 +3,9 @@ package com.gfg.strings;
 public class PalindromeCheck {
 
 	public static void main(String[] args) {
-		String str = "a";
+		String str = "madam";
 
-		System.out.println("String reversed or not? : " + isPalindrome(str));
+		System.out.println("String reversed or not? : " + isPal(str));
 	}
 
 	/*
@@ -21,14 +21,12 @@ public class PalindromeCheck {
 	}
 
 	/*
-	 * efficient approach
-	 * time complexity Big-O(n)
-	 * space complexity Big-O(1)
+	 * efficient approach time complexity Big-O(n) space complexity Big-O(1)
 	 */
 	static boolean isPal(String str) {
 		int begin = 0;
 		int end = str.length() - 1;
-		
+
 		while (begin < end) {
 			if (str.charAt(begin) != str.charAt(end))
 				return false;
---
+++ Strings/src/main/java/com/gfg/strings/SubstringSolution.java
@@ -0,0 +1,16 @@
+package com.gfg.strings;
+
+import java.util.Scanner;
+
+public class SubstringSolution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String S = in.next();
+		int start = in.nextInt();
+		int end = in.nextInt();
+		System.out.println(S.substring(start, end));
+		in.close();
+	}
+
+}
