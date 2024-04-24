4b1174714e5f0f7f777678aab0bf2be927355385
shoravs <sharmasaurav142@gmail.com>
2024-04-24 07:39:52 +0530

    add time complexity

--- Strings/src/main/java/com/gfg/strings/Anagram.java
+++ Strings/src/main/java/com/gfg/strings/Anagram.java
@@ -3,39 +3,44 @@ package com.gfg.strings;
 import java.util.Arrays;
 
 public class Anagram {
+
+	static final int CHAR = 122;
+
 	/*
 	 * time complexity Big-O(n log n)
 	 */
 	public static boolean isAnagram(String str1, String str2) {
+
 		if (str1.length() != str2.length())
 			return false;
 
 		char[] s1 = str1.toCharArray();
-		Arrays.sort(s1);
-		str1 = new String(s1);
-
 		char[] s2 = str2.toCharArray();
+
+		Arrays.sort(s1);
 		Arrays.sort(s2);
+
+		str1 = new String(s1);
 		str2 = new String(s2);
 
 		return str1.equals(str2);
 	}
 
 	/*
-	 * efficient approach
+	 * efficient approach Big-O(n) orBig-O(n + CHAR)
 	 */
 	public static boolean isAna(String str1, String str2) {
 		if (str1.length() != str2.length())
 			return false;
 
-		int[] count = new int[256];
+		int[] count = new int[CHAR];
 
 		for (int i = 0; i < str1.length(); i++) {
 			count[str1.charAt(i)]++;
 			count[str2.charAt(i)]--;
 		}
 
-		for (int i = 0; i < 256; i++)
+		for (int i = 0; i < CHAR; i++)
 			if (count[i] != 0)
 				return false;
 		return true;
