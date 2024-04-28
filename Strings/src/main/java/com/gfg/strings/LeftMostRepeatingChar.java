37a60c9cc04290d7db921f89f9b41a74ed1b23e7
shoravs <sharmasaurav142@gmail.com>
2024-04-28 17:25:24 +0530

    add better sol for find left most repeat char

--- Strings/src/main/java/com/gfg/strings/LeftMostRepeatingChar.java
+++ Strings/src/main/java/com/gfg/strings/LeftMostRepeatingChar.java
@@ -3,17 +3,32 @@ package com.gfg.strings;
 public class LeftMostRepeatingChar {
 
 	public static void main(String[] args) {
-		System.out.println(leftMostRepeatChar("aabc"));
+		System.out.println(leftMostRepeatChar2("babcc"));
 	}
 
 	/*
 	 * using Naive approach
 	 */
-	private static int leftMostRepeatChar(String s) {
+	private static int leftMostRepeatChar1(String s) {
 		for (int i = 0; i < s.length(); i++)
 			for (int j = i + 1; j < s.length(); j++)
 				if (s.charAt(i) == s.charAt(j))
 					return i;
+		return -1;
+	}
+
+	/*
+	 * better approach
+	 */
+	private static int leftMostRepeatChar2(String s) {
+		int[] count = new int[256];
+
+		for (int i = 0; i < s.length(); i++)
+			count[s.charAt(i)]++;
+
+		for (int i = 0; i < s.length(); i++)
+			if (count[s.charAt(i)] > 1)
+				return i;
 		return -1;
 	}
 
