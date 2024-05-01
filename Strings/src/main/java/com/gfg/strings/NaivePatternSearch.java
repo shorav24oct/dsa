3c5c33e4acecc88c09902015ef3df33e4bab161c
shoravs <sharmasaurav142@gmail.com>
2024-05-01 16:09:34 +0530

    naive sol for pattern search in String text

---
+++ Strings/src/main/java/com/gfg/strings/NaivePatternSearch.java
@@ -0,0 +1,15 @@
+package com.gfg.strings;
+
+public class NaivePatternSearch {
+
+	public static void main(String[] args) {
+		String text = "abcabcd";
+		String pattern = "bc";
+		patternSearch(pattern, text, pattern.length(), text.length());
+	}
+
+	private static void patternSearch(String pattern, String text, int m, int n) {
+		
+	}
+
+}
