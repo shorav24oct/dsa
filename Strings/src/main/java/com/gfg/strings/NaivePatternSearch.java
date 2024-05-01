03c9de06ee16706c75b7109e08ad4052c3927669
shoravs <sharmasaurav142@gmail.com>
2024-05-01 16:40:21 +0530

    naive sol for pattern search in String text

--- Strings/src/main/java/com/gfg/strings/NaivePatternSearch.java
+++ Strings/src/main/java/com/gfg/strings/NaivePatternSearch.java
@@ -3,13 +3,22 @@ package com.gfg.strings;
 public class NaivePatternSearch {
 
 	public static void main(String[] args) {
-		String text = "abcabcd";
-		String pattern = "bc";
+		String text = "aaaa";
+		String pattern = "aa";
 		patternSearch(pattern, text, pattern.length(), text.length());
 	}
 
 	private static void patternSearch(String pattern, String text, int m, int n) {
+		int j = 0, i = 0;
 		
+		for (i = 0; i <= n - m; i++) {
+			for (j = 0; j < m; j++) {
+				if (pattern.charAt(j) != text.charAt(i + j))
+					break;
+			}
+			if (j == m)
+				System.out.print(i + " ");
+		}
 	}
 
 }
