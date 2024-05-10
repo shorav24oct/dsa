76a5532394103d1346da12f6dba54e006fce21e1
shoravs <sharmasaurav142@gmail.com>
2024-05-10 17:54:06 +0530

    add efficient sol for push zero to end

--- Array/src/main/java/com/gfg/Array/MoveZerosToEnd.java
+++ Array/src/main/java/com/gfg/Array/MoveZerosToEnd.java
@@ -2,31 +2,53 @@ package com.gfg.Array;
 
 import java.util.Arrays;
 
-public class PushZero {
+public class MoveZerosToEnd {
 
 	public static void main(String[] args) {
-		int[] arr = {1, 2, 0, 0, 0, 3, 6};
-		
+		int[] arr = { 10, 5, 0, 0, 8, 0, 9, 0 };
+
 		System.out.println(Arrays.toString(arr));
-		
-		test(arr, arr.length);
+
+		moveZeroToEnd(arr, arr.length);
 	}
 
-	static void test(int[] arr, int n) {
-		int count = 0;
-
+	/*
+	 * Naive Solution
+	 */
+	static void moveToEnd(int[] arr, int n) {
 		for (int i = 0; i < n; i++) {
-			if (arr[i] != 0) {
-				arr[count++] = arr[i];
+			if (arr[i] == 0) {
+				for (int j = i + 1; j < n; j++) {
+					if (arr[j] != 0) {
+						swap(arr, i, j);
+						break;
+					}
+				}
+
 			}
 		}
-		
-		while(count < n) {
-			arr[count] = 0;
-			count++;
-		}
-		
 		System.out.println(Arrays.toString(arr));
+	}
+
+	/*
+	 * Efficient Solution
+	 */
+	static void moveZeroToEnd(int[] arr, int n) {
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			if (arr[i] != 0) {
+				swap(arr, count, i);
+				count++;
+			}
+
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+
+	private static void swap(int A[], int a, int b) {
+		int temp = A[a];
+		A[a] = A[b];
+		A[b] = temp;
 	}
 
 }
--- Array/src/main/java/com/gfg/Array/RemoveDuplicates.java
+++ Array/src/main/java/com/gfg/Array/RemoveDuplicates.java
@@ -7,12 +7,11 @@ public class RemoveDuplicates {
 	public static void main(String[] args) {
 		int[] arr = { 10, 20, 20, 30, 30, 30 };
 		System.out.println("Before: " + Arrays.toString(arr) + " Size: " + arr.length);
-		removeDup(arr);
+		removeDuplicates(arr);
 	}
 
 	/*
-	 * Naive Solution Theta-n 
-	 * Aux Space Theta-n
+	 * Naive Solution time complexity: Theta-n & Aux Space Theta-n
 	 */
 	static void removeDup(int[] arr) {
 		int[] temp = new int[arr.length];
@@ -22,7 +21,7 @@ public class RemoveDuplicates {
 		int res = 1;
 
 		for (int i = 1; i < arr.length; i++) {
-			if (temp[res-1] != arr[i]) {
+			if (temp[res - 1] != arr[i]) {
 				temp[res] = arr[i];
 				res++;
 			}
@@ -30,9 +29,23 @@ public class RemoveDuplicates {
 
 		for (int i = 0; i < arr.length; i++)
 			arr[i] = temp[i];
-		
-		System.out.println("After: " + Arrays.toString(arr) + " Size: " + res);
 
+		System.out.println("After: " + Arrays.toString(arr) + " Size: " + res);
+	}
+
+	/*
+	 * Efficient Solution time complexity: Theta-n & Aux Space Theta-1
+	 */
+	static void removeDuplicates(int[] arr) {
+		int res = 1;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[res - 1] != arr[i]) {
+				arr[res] = arr[i];
+				res++;
+			}
+		}
+		System.out.println("After: " + Arrays.toString(arr) + " Size: " + res);
 	}
 
 }
