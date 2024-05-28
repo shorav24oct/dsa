530c2fa803904ce2279bafd2adbc73b449b4fc92
shoravs <sharmasaurav142@gmail.com>
2024-05-28 06:18:27 +0530

    last occurrence of no in array

---
+++ Searching/src/main/java/com/gfg/Searching/LastOccurrence.java
@@ -0,0 +1,33 @@
+package com.gfg.Searching;
+
+public class LastOccurrence {
+
+	public static void main(String[] args) {
+		int[] arr = { 5, 10, 10, 15, 15, 15 };
+		int numberToBeSearch = 15;
+		System.out.println("Number " + numberToBeSearch + " is present at index: " + search(arr, numberToBeSearch));
+	}
+
+	private static int search(int[] arr, int numberToBeSearch) {
+		int low = 0;
+		int high = arr.length - 1;
+
+		while (low <= high) {
+			int mid = (low + high) / 2;
+
+			if (arr[mid] > numberToBeSearch)
+				high = mid - 1;
+			else if (arr[mid] < numberToBeSearch)
+				low = mid + 1;
+			else {
+				if (mid == arr.length - 1 || arr[mid] != arr[mid + 1])
+					return mid;
+				else
+					low = mid + 1;
+			}
+		}
+
+		return -1;
+	}
+
+}
