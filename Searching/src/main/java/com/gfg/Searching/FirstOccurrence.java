0e6e200240938236315f4fee8aff808e8d9fa69a
shoravs <sharmasaurav142@gmail.com>
2024-05-28 06:07:53 +0530

    first occurrence of no in array

--- Searching/src/main/java/com/gfg/Searching/FirstOccurrence.java
+++ Searching/src/main/java/com/gfg/Searching/FirstOccurrence.java
@@ -1,6 +1,7 @@
 package com.gfg.Searching;
 
-public class BinarySearch {
+public class FirstOccurrence {
+
 	public static void main(String[] args) {
 		int[] arr = { 5, 10, 10, 15, 15, 15 };
 		int numberToBeSearch = 15;
@@ -14,14 +15,19 @@ public class BinarySearch {
 		while (low <= high) {
 			int mid = (low + high) / 2;
 
-			if (arr[mid] == numberToBeSearch)
-				return mid;
-			else if (arr[mid] > numberToBeSearch)
+			if (arr[mid] > numberToBeSearch)
 				high = mid - 1;
-			else
+			else if (arr[mid] < numberToBeSearch)
 				low = mid + 1;
+			else {
+				if (mid == 0 || arr[mid - 1] != arr[mid])
+					return mid;
+				else
+					high = mid - 1;
+			}
 		}
 
 		return -1;
 	}
+
 }
