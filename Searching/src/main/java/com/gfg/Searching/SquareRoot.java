4664d469507ca5841f70a5bdea17d6f70c374584
shoravs <sharmasaurav142@gmail.com>
2024-05-29 06:36:57 +0530

    add efficient sol

--- Searching/src/main/java/com/gfg/Searching/SquareRoot.java
+++ Searching/src/main/java/com/gfg/Searching/SquareRoot.java
@@ -3,8 +3,8 @@ package com.gfg.Searching;
 public class SquareRoot {
 
 	public static void main(String[] args) {
-		int no = 81;
-		System.out.println("Square root of " + no + " is " + sqRoot(no));
+		int no = 16;
+		System.out.println("Square root of " + no + " is " + sqRootEfficient(no));
 	}
 
 	/*
@@ -17,6 +17,27 @@ public class SquareRoot {
 			i++;
 
 		return (i - 1);
+	}
+
+	private static int sqRootEfficient(int no) {
+		int low = 0;
+		int high = no;
+		int ans = 0;
+
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			int midSq = mid * mid;
+
+			if (midSq == no)
+				return mid;
+			else if (midSq > no)
+				high = mid - 1;
+			else {
+				low = mid + 1;
+				ans = mid;
+			}
+		}
+		return ans;
 	}
 
 }
