a4ba9b37340f90c429f4f06a9d80bce8c5ae5b5e
shoravs <sharmasaurav142@gmail.com>
2024-05-31 15:33:38 +0530

    bubble sort

--- Sorting/src/main/java/com/gfg/Sorting/BubbleSort.java
+++ Sorting/src/main/java/com/gfg/Sorting/BubbleSort.java
@@ -7,6 +7,7 @@ public class BubbleSort {
 		int[] arr = { 5, 10, 8, 6 };
 		System.out.println("Array before Sort : " + Arrays.toString(arr));
 		sort(arr, arr.length);
+		System.out.println("Array after Sort : " + Arrays.toString(arr));
 	}
 
 	private static void sort(int[] arr, int length) {
@@ -14,14 +15,18 @@ public class BubbleSort {
 			boolean swapped = false;
 			for (int j = 0; j < length - i - 1; j++) {
 				if (arr[j] > arr[j + 1]) {
-					swap(arr[j], arr[j + 1]);
+					swap(arr, j);
 					swapped = true;
 				}
 			}
+			if (swapped == false)
+				break;
 		}
 	}
 
-	private static void swap(int i, int j) {
-		
+	private static void swap(int[] arr, int j) {
+		int temp = arr[j];
+		arr[j] = arr[j + 1];
+		arr[j + 1] = temp;
 	}
 }
