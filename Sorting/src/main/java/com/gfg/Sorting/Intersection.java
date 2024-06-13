efd1578e1cfd874690e0430ac08022e5058ee269
shoravs <sharmasaurav142@gmail.com>
2024-06-13 05:30:45 +0530

    intersection of 2 sorted arrays

---
+++ Sorting/src/main/java/com/gfg/Sorting/Intersection.java
@@ -0,0 +1,15 @@
+package com.gfg.Sorting;
+
+public class Intersection {
+
+	public static void main(String[] args) {
+		int[] a = { 5, 10, 20, 20 };
+		int[] b = { 2, 20, 20, 20 };
+		intersect(a, b, a.length, b.length);
+	}
+
+	private static void intersect(int[] a, int[] b, int m, int n) {
+		
+	}
+
+}
