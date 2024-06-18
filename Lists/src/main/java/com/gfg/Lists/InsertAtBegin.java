5a4672ffe2c61823daf498044bfada565f67c789
shoravs <sharmasaurav142@gmail.com>
2024-06-18 20:00:01 +0530

    insert at begin

--- Lists/src/main/java/com/gfg/Lists/InsertAtBegin.java
+++ Lists/src/main/java/com/gfg/Lists/InsertAtBegin.java
@@ -3,7 +3,17 @@ package com.gfg.Lists;
 public class InsertAtBegin {
 
 	public static void main(String[] args) {
+		Node head = null;
 		
+		head = insertBegin(head, 30);
+		head = insertBegin(head, 20);
+		head = insertBegin(head, 10);
+	}
+	
+	static Node insertBegin(Node head, int x) {
+		Node temp = new Node(x, head);
+		temp.next = head;
+		return temp;
 	}
 
 }
