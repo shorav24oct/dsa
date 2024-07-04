10ba74baf7ac2d5cf44847582f4b8843986ed182
shoravs <sharmasaurav142@gmail.com>
2024-07-04 06:05:01 +0530

    check is text anagram or not

--- Revision1/src/main/java/com/gfg/strings/AnagramCheck.java
+++ Revision1/src/main/java/com/gfg/strings/AnagramCheck.java
@@ -3,11 +3,17 @@ package com.gfg.strings;
 public class AnagramCheck {
 
 	public static void main(String[] args) {
-		System.out.println(isAna("mat"));
+		System.out.println(isAna1("mat", "tam"));
+		System.out.println(isAna2("cat", "atc"));
 	}
-	
-	static boolean isAna(String text) {
-		
+
+	static boolean isAna1(String s1, String s2) {
+
+		return false;
+	}
+
+	static boolean isAna2(String s1, String s2) {
+
 		return false;
 	}
 
