90e693a1ad5a661cb0e2c529b9c48223a4f8092c
shoravs <sharmasaurav142@gmail.com>
2024-07-05 05:50:03 +0530

    print sub string using pre defined substring method

--- Revision1/src/main/java/com/gfg/strings/PrintSubstrings.java
+++ Revision1/src/main/java/com/gfg/strings/PrintSubstrings.java
@@ -3,16 +3,23 @@ package com.gfg.strings;
 public class PrintSubstrings {
 
 	public static void main(String[] args) {
-		printSubStr("abc");
+		printSubStr2("abc");
 	}
-	
-	static void printSubStr(String str) {
+
+	static void printSubStr1(String str) {
 		for (int i = 0; i < str.length(); i++) {
 			String subStr = "";
 			for (int j = i; j < str.length(); j++) {
 				subStr += str.charAt(j);
 				System.out.println(subStr);
 			}
+		}
+	}
+
+	static void printSubStr2(String str) {
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = i + 1; j <= str.length(); j++)
+				System.out.println(str.substring(i, j));
 		}
 	}
 
