5a6853110d11e39d57d100cfd9fd248cc0ae1684
shoravs <sharmasaurav142@gmail.com>
2024-07-09 06:18:01 +0530

    check string is rotatable or not

--- Revision1/src/main/java/com/gfg/strings/CheckRotation.java
+++ Revision1/src/main/java/com/gfg/strings/CheckRotation.java
@@ -3,7 +3,17 @@ package com.gfg.strings;
 public class CheckRotation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(rotable("ABCD", "DABc"));
+	}
+
+	static boolean rotable(String str1, String str2) {
+		if (str1.length() != str2.length())
+			return false;
+
+		if ((str1 + str1).indexOf(str2) >= 0)
+			return true;
+		else
+			return false;
 
 	}
 
