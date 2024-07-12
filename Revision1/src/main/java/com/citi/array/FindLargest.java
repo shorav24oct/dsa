88365de48eec2e3f72d1c9dacdae755e19329d40
shoravs <sharmasaurav142@gmail.com>
2024-07-12 06:04:15 +0530

    find largest

--- Revision1/src/main/java/com/citi/array/FindLargest.java
+++ Revision1/src/main/java/com/citi/array/FindLargest.java
@@ -3,7 +3,30 @@ package com.citi.array;
 public class FindLargest {
 
 	public static void main(String[] args) {
-		
+		int[] arr = { 10, 5, 20, 80 };
+
+		System.out.println(largestNaive(arr));
+	}
+
+	/*
+	 * Naive using 2 for loop
+	 */
+	static int largestNaive(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+
+			boolean flag = true;
+
+			for (int j = 0; j < arr.length; j++) {
+				if (arr[j] > arr[i]) {
+					flag = false;
+					break;
+				}
+			}
+
+			if (flag == true)
+				return i;
+		}
+		return -1;
 	}
 
 }
